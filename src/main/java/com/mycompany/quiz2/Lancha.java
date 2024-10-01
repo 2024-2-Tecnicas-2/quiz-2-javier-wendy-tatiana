0553f82816bfaee8309e508e80a58387483573cc
Cristiano Ronalgon <cristianoronalgon@gmail.com>
2024-09-30 20:49:05 -0500

    Lancha

---
+++ src/main/java/com/mycompany/quiz2/Lancha.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.quiz2;
+
+public class Lancha extends VehiculoBase {
+    private TipoCombustible combustible;
+
+    public Lancha(String marca, String modelo, TipoCombustible combustible) {
+        super(marca, modelo);
+        this.combustible = combustible;
+    }
+
+    @Override
+    public TipoCombustible tipoCombustible() {
+        return combustible;
+    }
+}
+
