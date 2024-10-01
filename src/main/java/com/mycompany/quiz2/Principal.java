e5caa038c38a7feb850bc97a3e6597dcfeeb1e9c
zavithar <mohammedali@gmail.com>
2024-09-30 20:29:54 -0500

    corrección primer punto

--- src/main/java/com/mycompany/quiz2/Principal.java
+++ src/main/java/com/mycompany/quiz2/Principal.java
@@ -3,6 +3,14 @@ package com.mycompany.quiz2;
 public class Principal {
 
     public static void main(String[] args) {
+   
         // TODO: Aquí va tu código
     }
+    public class Vehiculo {
+    private TipoCombustible TipoCombustible;
+
+    public void setTipoCombustible(TipoCombustible tipoCombustible) {
+        this.TipoCombustible = tipoCombustible;
+    }
+  }
 }
--- src/main/java/com/mycompany/quiz2/TipoCombustible.java
+++ src/main/java/com/mycompany/quiz2/TipoCombustible.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.quiz2;
 
-/**
- *
- * @author USUARIO
- */
-public class TipoCombustible {
-    
+package com.mycompany.quiz2;
+  public enum TipoCombustible {
+    GASOLINA,
+    DIESEL,
+    ELECTRICO
 }
+
+    
